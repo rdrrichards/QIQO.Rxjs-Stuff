eade71bcb5ca363201e3ac10c359f8e80dcef3b7
R. Richards <rdrrichards@hotmail.com>
2020-01-17 17:16:28 -0600

    unit test fixes

--- src/app/star-wars/star-wars.component.spec.ts
+++ src/app/star-wars/star-wars.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { StarWarsComponent } from './star-wars.component';
 
@@ -8,7 +9,8 @@ describe('StarWarsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StarWarsComponent ]
+      declarations: [ StarWarsComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   }));
